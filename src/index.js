833fb86a4da692f80f37152581408622858a3501
antoanstin <antoanstin@gmail.com>
2023-08-01 15:32:15 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Routing from './Router/Routing';
+// import Routing from './Router/Routing';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    <Routing/>
+    <App/>
   
 );
 
